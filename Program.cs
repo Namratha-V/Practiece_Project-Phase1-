c0350004976f7561c6de5a78c8f3e3dac74e0185
V <nv@dovercorp.com>
2021-09-04 08:00:03 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace SelfLearning_Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fpath = @"C:\Users\11033259\Desktop\fresher_training\Student_data.txt";
+            StreamReader sr = new StreamReader(fpath);
+            string fileContent = sr.ReadToEnd();
+            Console.WriteLine(fileContent);
+            sr.Close();
+        }
+    }
+}
